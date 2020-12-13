8c3f3111dd255bbf76692340dd559c6f7c23f508
oxalisviolacea <hannadavis09@gmail.com>
2020-12-12 18:40:59 -0700

    Add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,25 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'csv'
+cmd = "pg_restore --verbose --clean --no-acl --no-owner -h localhost -U $(whoami) -d rails_engine_development db/data/rails-engine-development.pgdump"
+puts "Loading PostgreSQL Data dump into local database with command:"
+puts cmd
+system(cmd)
+
+csv_text = File.read(Rails.root.join("db", "data", "items.csv"))
+csv = CSV.parse(csv_text, :headers => true, :encoding => "ISO-8859-1")
+csv.each do |row|
+  i = Item.new
+  i.id = row["id"]
+  i.name = row["name"]
+  i.description = row["description"]
+  i.unit_price = (row["unit_price"].to_f / 100)
+  i.merchant_id = row["merchant_id"]
+  i.created_at = row["created_at"]
+  i.updated_at = row["updated_at"]
+  i.save
+end
+
+puts "There are now #{Item.count} rows in the items table"
+
+ActiveRecord::Base.connection.tables.each do |t|
+  ActiveRecord::Base.connection.reset_pk_sequence!(t)
+end
