96ab73d6273ee3c5e631eb00f46a6b358bb6166f
oxalisviolacea <hannadavis09@gmail.com>
2020-12-14 21:36:21 -0700

    Lint code

--- spec/requests/api/v1/merchant_request_spec.rb
+++ spec/requests/api/v1/merchant_request_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
-describe "Merchants API" do
-  it "can send a list of merchants" do
+describe 'Merchants API' do
+  it 'can send a list of merchants' do
     create_list(:merchant, 3)
 
     get '/api/v1/merchants'
@@ -9,7 +9,7 @@ describe "Merchants API" do
     expect(response).to be_successful
 
     merchants = JSON.parse(response.body, symbolize_names: true)
-    
+
     merchants.each do |merchant|
       expect(merchant).to have_key(:id)
       expect(merchant[:id]).to be_an(Integer)
@@ -25,7 +25,7 @@ describe "Merchants API" do
     end
   end
 
-  it "can get one merchant by its id" do
+  it 'can get one merchant by its id' do
     id = create(:merchant).id
 
     get "/api/v1/merchants/#{id}"
@@ -47,11 +47,11 @@ describe "Merchants API" do
     expect(merchant[:updated_at]).to be_a(String)
   end
 
-  it "can create a new merchant" do
+  it 'can create a new merchant' do
     merchant_params = {
-                        name: 'Test Merchant Name'
-                      }
-    headers = {"CONTENT_TYPE" => "application/json"}
+      name: 'Test Merchant Name'
+    }
+    headers = { 'CONTENT_TYPE' => 'application/json' }
 
     post '/api/v1/merchants', headers: headers, params: JSON.generate(merchant_params)
     created_merchant = Merchant.last
@@ -62,21 +62,21 @@ describe "Merchants API" do
     expect(created_merchant.updated_at).not_to be_empty
   end
 
-  it "can update an existing merchant" do
+  it 'can update an existing merchant' do
     id = create(:merchant).id
     previous_name = Merchant.last.name
-    merchant_params = { name: "Updated Merchant Name" }
-    headers = {"CONTENT_TYPE" => "application/json"}
+    merchant_params = { name: 'Updated Merchant Name' }
+    headers = { 'CONTENT_TYPE' => 'application/json' }
 
     patch "/api/v1/merchants/#{id}", headers: headers, params: JSON.generate(merchant_params)
     merchant = Merchant.find_by(id: id)
 
     expect(response).to be_successful
     expect(merchant.name).to_not eq(previous_name)
-    expect(merchant.name).to eq("Updated Merchant Name")
+    expect(merchant.name).to eq('Updated Merchant Name')
   end
 
-  it "can destroy an merchant" do
+  it 'can destroy an merchant' do
     merchant = create(:merchant)
 
     expect(Merchant.count).to eq(1)
@@ -85,6 +85,6 @@ describe "Merchants API" do
 
     expect(response).to be_successful
     expect(Merchant.count).to eq(0)
-    expect{Merchant.find(merchant.id)}.to raise_error(ActiveRecord::RecordNotFound)
+    expect { Merchant.find(merchant.id) }.to raise_error(ActiveRecord::RecordNotFound)
   end
 end
