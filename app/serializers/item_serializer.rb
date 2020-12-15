af0b31f076b86a2da25cfb3ff4e715fc280235fb
oxalisviolacea <hannadavis09@gmail.com>
2020-12-15 14:13:25 -0700

    Add items serializer with relationships

---
+++ app/serializers/item_serializer.rb
@@ -0,0 +1,8 @@
+class ItemSerializer
+  include FastJsonapi::ObjectSerializer
+  attributes :name, :description, :unit_price, :merchant_id
+
+  belongs_to :merchant
+  has_many :invoice_items
+  has_many :invoices, through: :invoice_items
+end
