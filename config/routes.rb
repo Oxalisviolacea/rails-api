bd99089d50d9efb1010da92afde595f329e473d3
oxalisviolacea <hannadavis09@gmail.com>
2020-12-15 19:28:03 -0700

    Add route for merchant/items

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
   namespace 'api' do
     namespace 'v1' do
-      resources :merchants
-      resources :items
+      resources :merchants do
+        # resources :items, only: [:index]
+        get '/items', to: 'merchants/items#index'
+      end
+      resources :items do
+        resources :merchants, only: [:index]
+      end
     end
   end
 end
