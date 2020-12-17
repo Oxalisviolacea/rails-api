484870c18d90d57e1592d5403deb9773c0b8ce29
oxalisviolacea <hannadavis09@gmail.com>
2020-12-16 18:42:47 -0700

    Add routes for merchant find and find_all

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   namespace 'api' do
     namespace 'v1' do
       resources :merchants do
+        collection do
+          get '/find', to: 'merchants/search#show'
+          get '/find_all', to: 'merchants/search#index'
+        end
         get '/items', to: 'merchants/items#index'
       end
       resources :items do
