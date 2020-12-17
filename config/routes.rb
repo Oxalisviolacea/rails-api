100f07131a76d2e351e2369e197fb4bcbb7ac58b
oxalisviolacea <hannadavis09@gmail.com>
2020-12-16 18:19:54 -0700

    Add rotue for items search

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,14 @@ Rails.application.routes.draw do
   namespace 'api' do
     namespace 'v1' do
       resources :merchants do
-        # resources :items, only: [:index]
         get '/items', to: 'merchants/items#index'
       end
       resources :items do
-        resources :merchants, only: [:index]
+        collection do
+          get '/find', to: 'items/search#show'
+          get '/find_all', to: 'items/search#index'
+        end
+        get '/merchants', to: 'items/merchants#index'
       end
     end
   end
