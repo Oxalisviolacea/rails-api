851b2e7384c33ddaa5ec7d1f964ae3bbdf52d067
oxalisviolacea <hannadavis09@gmail.com>
2020-12-17 19:15:19 -0700

    Remove transaction relationship

--- app/models/invoice_item.rb
+++ app/models/invoice_item.rb
@@ -1,7 +1,6 @@
 class InvoiceItem < ApplicationRecord
   belongs_to :item
   belongs_to :invoice
-  has_many :transactions
   validates :quantity, presence: true
   validates :unit_price, presence: true
 end
