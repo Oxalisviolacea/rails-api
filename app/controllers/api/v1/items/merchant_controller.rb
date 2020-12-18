96b76005145570115e944df8dee73c571964a902
oxalisviolacea <hannadavis09@gmail.com>
2020-12-17 19:13:47 -0700

    Add s to file name

--- app/controllers/api/v1/items/merchant_controller.rb
+++
@@ -1,6 +0,0 @@
-class Api::V1::Items::MerchantsController < ApplicationController
-  def index
-    item = Item.find(params[:id])
-    render json: MerchantSerializer.new(Merchant.find(item.merchant_id))
-  end
-end
