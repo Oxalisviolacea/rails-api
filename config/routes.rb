445bf008ffff228d58865172bfae3565480a3f48
oxalisviolacea <hannadavis09@gmail.com>
2020-12-17 22:20:16 -0700

    Add route for a merchant's revenue

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
   namespace 'api' do
     namespace 'v1' do
+      get '/revenue', to: 'revenue#total_revenue'
+      get '/merchants/:id/revenue', to: 'revenue#revenue_for_merchant'
       resources :merchants do
         collection do
           get '/find', to: 'merchants/search#show'
           get '/find_all', to: 'merchants/search#index'
           get '/most_revenue', to: 'merchants#merchant_with_most_revenue'
-          get '/most_items', to: 'merchants#merchant_with_most_items_sold'
         end
         get '/items', to: 'merchants/items#index'
       end
